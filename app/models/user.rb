0de8800630de7348cbfc30188c2df40f2b0ef8a5
Graham Rich <richgr@gmail.com>
2011-11-07 18:09:40 -0400

    Database configured.  Generated a Users model.  First attempt at RSpec models tests. v02

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ActiveRecord::Base
+
+  attr_accessible :first_name, :middle_name, :last_name
+  
+  validates :first_name, :presence => true,
+                        :length => 2..50
+  validates :middle_name, :presence => true,
+                        :length => 2..50
+  validates :last_name, :presence => true,
+                        :length => 2..50
+
+end
